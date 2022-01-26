57990f01aa0f0b986392cd9e0b8d7bb2e2ab59bd
Brian Aguirre <aguirre.28@osu.edu>
2022-01-26 08:22:19 -0500

    implement Add Course form elements in scroll view with keyboard view adjustment and text field validation

--- FooDisc/View Controllers/AddCourseViewController.swift
+++ FooDisc/View Controllers/AddCourseViewController.swift
@@ -5,8 +5,6 @@
 //  Created by Aguirre, Brian P. on 1/25/22.
 //
 
-// TODO: add keyboard view adjustment/dismissal
-
 import UIKit
 
 class AddCourseViewController: UIViewController {
@@ -17,14 +15,31 @@ class AddCourseViewController: UIViewController {
     @IBOutlet var numberOfHolesTextField: UITextField!
     @IBOutlet var latitudeTextField: UITextField!
     @IBOutlet var longitudeTextField: UITextField!
-    @IBOutlet var submitButton: UIButton!
+    @IBOutlet var doneButton: UIBarButtonItem!
+    @IBOutlet var scrollView: UIScrollView!
     
     var textFields: [UITextField] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        submitButton.isEnabled = false
+        initializeForm()
+    }
+    
+    // TODO: create Course object
+    // TODO: verify that this IBAction is the best way to handle saving Course and navigating back to courses view (maybe unwind stuff?)
+    @IBAction func doneButtonPressed(_ sender: Any) {
+        
+    }
+    
+    // MARK: Form initialization functions
+    
+    func initializeForm() {
+        // Disable done button until all fields are filled in
+        doneButton.isEnabled = false
+        
+        // Add listener for keyboard notifications
+        registerForKeyboardNotifications()
 
         // Add listener for textField validation
         textFields = [courseNameTextField, cityTextField, stateTextField, numberOfHolesTextField, latitudeTextField, longitudeTextField]
@@ -37,18 +52,39 @@ class AddCourseViewController: UIViewController {
     @objc func textFieldDidChange(_ textField: UITextField) {
         for textField in textFields {
             if !textField.hasText {
+                if doneButton.isEnabled {
+                    doneButton.isEnabled = false
+                }
                 break
             }
             if textField == textFields.last {
-                submitButton.isEnabled = true
+                doneButton.isEnabled = true
             }
         }
     }
     
-    // TODO: create Coure object
-    // TODO: verify that this IBAction is the best way to handle saving Course and navigating back to courses view (maybe unwind stuff?)
-    @IBAction func submitButtonPressed(_ sender: Any) {
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(_:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWasShown(_ notificiation: NSNotification) {
+        guard let info = notificiation.userInfo, let keyboardFrameValue = info[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
         
+        let keyboardFrame = keyboardFrameValue.cgRectValue
+        let keyboardSize = keyboardFrame.size
+        
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0,
+        bottom: keyboardSize.height, right: 0.0)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc func keyboardWillBeHidden(_ notification:
+       NSNotification) {
+        let contentInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
     }
     
 }
